eed20fc5ed322c855465d887efc4af58ded90f39
grace7937 <66059987+grace7937@users.noreply.github.com>
2020-12-25 22:39:30 +0900

    js: complet returnResultAfterTowSec fuc

--- mission1/step-3.js
+++ mission1/step-3.js
@@ -1,4 +1,4 @@
-var DATA = [
+const DATA = [
   {
     name: '데이터베이스',
     grade: 'A',
@@ -19,7 +19,7 @@ var DATA = [
   },
 ];
 
-var GRADE_MAP = {
+const GRADE_MAP = {
   'A+': 4.5,
   A: 4,
   'B+': 3.5,
@@ -34,10 +34,10 @@ var GRADE_MAP = {
 // 전공이수학점이 얼마인지, 전공평점이 얼마인지
 // > 총평점 : 3.92, 전공평점:3.5, 이수학점:12, 전공이수학점:6
 
-let getTotalGradeAndToTalCredit = function (DATA) {
-  const gradeInfo = DATA.reduce(
+const getGradeInfo = function (data) {
+  const gradeInfo = data.reduce(
     (accumulator, currentValue) => {
-      let result = {
+      const result = {
         totalGrade:
           accumulator.totalGrade +
           GRADE_MAP[currentValue.grade] * currentValue.credit,
@@ -52,27 +52,21 @@ let getTotalGradeAndToTalCredit = function (DATA) {
   return gradeInfo;
 };
 
-console.log(getTotalGradeAndToTalCredit(DATA));
-
 //이수학점
-let getTotalCredit = function (DATA) {
-  const result = getTotalGradeAndToTalCredit(DATA).totalCredit;
-  return result;
+const getTotalCredit = function (data) {
+  const { totalCredit } = getGradeInfo(data);
+  return totalCredit;
 };
-
-console.log('이수학점' + getTotalCredit(DATA));
 
 //총평점
-let getTotalGrade = function (DATA) {
-  const result =
-    getTotalGradeAndToTalCredit(DATA).totalGrade / getTotalCredit(DATA);
-  return result;
+const getTotalGrade = function (data) {
+  const { totalGrade } = getGradeInfo(data);
+  return totalGrade / getTotalCredit(data);
 };
-console.log('총평점' + getTotalGrade(DATA));
 
 //전공평점
-let getMajorCredit = function (DATA) {
-  let majorCredit = DATA.reduce((accumulator, currentValue) => {
+const getMajorCredit = function (data) {
+  const majorCredit = data.reduce((accumulator, currentValue) => {
     if (currentValue.major === true) {
       return accumulator + currentValue.credit;
     }
@@ -81,12 +75,10 @@ let getMajorCredit = function (DATA) {
   return majorCredit;
 };
 
-console.log('전공평점' + getMajorCredit(DATA));
-
 //전공이수학점
-let getMajorGrade = function (DATA) {
+const getMajorGrade = function (data) {
   let accumulatedGrade = 0;
-  DATA.forEach((currentValue) => {
+  data.forEach((currentValue) => {
     if (currentValue.major === true) {
       accumulatedGrade = accumulatedGrade + currentValue.credit;
     }
@@ -94,25 +86,54 @@ let getMajorGrade = function (DATA) {
   return accumulatedGrade;
 };
 
-console.log('전공이수학점' + getMajorGrade(DATA));
+// const gmg = (data) => data.filter(v => v.major).reduce(acc, v => acc + v.credit, 0)
 
 // 4.5 학점 to 4.0
-let convertCredit = function () {
-  let result = 0;
-  result = 60 + ((getMajorCredit(DATA) - 1) * 40) / 3.5;
+const convertCredit = function () {
+  const result = 60 + ((getMajorCredit(DATA) - 1) * 40) / 3.5;
 
   return result;
 };
 
-console.log('백분율로 환산하면' + convertCredit() + '입니다.');
-
 // 과목 추가 함수
 
-let addLecture = function () {
-  let newLecture = { name: '알고리즘', grade: 'B', credit: 3, Major: true };
-  DATA.push(newLecture);
+const addLecture = function (gradeData, name, grade, credit, major) {
+  const newLecture = { name, grade, credit, major };
+  return [...gradeData, newLecture]; // immutable (with spread operator);
 };
-console.log(addLecture());
-console.log(DATA);
 
 
+const returnResult = function () {
+    const gradeData = addLecture(DATA, '알고리즘', 'A', 3, true);
+    
+    return (
+        '이수학점' +
+        getTotalCredit(gradeData) +
+        '  총평점' +
+        getTotalGrade(gradeData) +
+        '  전공평점' +
+        getMajorCredit(gradeData) +
+        '  전공이수학점' +
+        getMajorGrade(gradeData) +
+        '  백분율로 환산하면' +
+        convertCredit() +
+        '입니다.'
+        );
+    };
+    
+    //2초후 결과 출력
+
+    const returnResultAfterTowSec = function() {
+        setTimeout(() => {
+            console.log(returnResult(DATA));
+            
+        }, 2000);
+    };
+    
+    
+    returnResultAfterTowSec();
+
+    
+    
+    
+    
